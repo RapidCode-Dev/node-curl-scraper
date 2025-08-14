ea53543818f6fde941c4954705d1090ebeda6756
= <louis.gaebele@web.de>
2025-08-14 13:43:57 +0800

    adding more fingerprints + explicit header option

--- dist/fingerprint-config.js
+++ dist/fingerprint-config.js
@@ -7,6 +7,7 @@ exports.findFingerprintByBrowser = findFingerprintByBrowser;
 exports.findFingerprintsByBrowser = findFingerprintsByBrowser;
 // Predefined fingerprint configurations
 exports.FINGERPRINT_CONFIGS = {
+    // ANDROID
     'chrome131-android': {
         name: 'Chrome 131 Android',
         browser: 'chrome',
@@ -45,6 +46,7 @@ exports.FINGERPRINT_CONFIGS = {
             tlsSignedCertTimestamps: true
         }
     },
+    // MACOS
     'chrome120-macos': {
         name: 'Chrome 120 macOS',
         browser: 'chrome',
@@ -79,6 +81,82 @@ exports.FINGERPRINT_CONFIGS = {
             certCompression: 'brotli',
             tlsGrease: true,
             tlsUseNewAlpsCodepoint: true,
+            tlsSignedCertTimestamps: true
+        }
+    },
+    'chrome124-macos': {
+        name: 'Chrome 124 macOS',
+        browser: 'chrome',
+        version: '124',
+        platform: 'desktop',
+        os: 'macos',
+        headers: {
+            'Sec-Ch-Ua': '"Chromium";v="124", "Google Chrome";v="124", "Not-A.Brand";v="99"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"macOS"',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-User': '?1',
+            'Sec-Fetch-Dest': 'document',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Priority': 'u=0, i'
+        },
+        tls: {
+            ciphers: 'TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA:AES256-SHA',
+            curves: 'X25519Kyber768Draft00:X25519:P-256:P-384',
+            http2Settings: '1:65536;2:0;4:6291456;6:262144',
+            http2WindowUpdate: 15663105,
+            http2StreamWeight: 256,
+            http2StreamExclusive: 1,
+            echGrease: true,
+            tlsv12: true,
+            alps: true,
+            tlsPermuteExtensions: true,
+            certCompression: 'brotli',
+            tlsGrease: true,
+            tlsUseNewAlpsCodepoint: false,
+            tlsSignedCertTimestamps: true
+        }
+    },
+    'chrome127-macos': {
+        name: 'Chrome 127 macOS',
+        browser: 'chrome',
+        version: '127',
+        platform: 'desktop',
+        os: 'macos',
+        headers: {
+            'Sec-Ch-Ua': '"Chromium";v="127", "Google Chrome";v="127", "Not-A.Brand";v="99"',
+            'Sec-Ch-Ua-Mobile': '?0',
+            'Sec-Ch-Ua-Platform': '"macOS"',
+            'Upgrade-Insecure-Requests': '1',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+            'Sec-Fetch-Site': 'none',
+            'Sec-Fetch-Mode': 'navigate',
+            'Sec-Fetch-User': '?1',
+            'Sec-Fetch-Dest': 'document',
+            'Accept-Encoding': 'gzip, deflate, br, zstd',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Priority': 'u=0, i'
+        },
+        tls: {
+            ciphers: 'TLS_AES_128_GCM_SHA256:TLS_AES_256_GCM_SHA384:TLS_CHACHA20_POLY1305_SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-RSA-AES128-SHA:ECDHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA:AES256-SHA',
+            curves: 'X25519Kyber768Draft00:X25519:P-256:P-384',
+            http2Settings: '1:65536;2:0;4:6291456;6:262144',
+            http2WindowUpdate: 15663105,
+            http2StreamWeight: 256,
+            http2StreamExclusive: 1,
+            echGrease: true,
+            tlsv12: true,
+            alps: true,
+            tlsPermuteExtensions: true,
+            certCompression: 'brotli',
+            tlsGrease: true,
+            tlsUseNewAlpsCodepoint: false,
             tlsSignedCertTimestamps: true
         }
     },
